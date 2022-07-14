622baa9f685e2525a2db517fce2d1780bafa9856
Henrique Barbosa <henrikleod@gmail.com>
2022-07-14 16:32:19 -0300

    feature: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import Navbar from "./components/navbar";
+import HealthCheck from "./components/HealthCheck";
+import Navbar from "./components/Navbar";
+import GlobalStyle from "./styles/globalStyle";
 
 function App() {
   return (
+    <>
+      <GlobalStyle />
       <Navbar/>
+      <HealthCheck/>
+    </>
   );
 }
 
---
+++ src/styles/globalStyle.tsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #f6f8fa;
+  }
+`;
+
+export default GlobalStyle;
