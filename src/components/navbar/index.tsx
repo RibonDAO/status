d2574acb96b921ca6a60aabfc83941f2460c61a9
Henrique Barbosa <henrikleod@gmail.com>
2022-07-14 11:04:34 -0300

    feature: navbar structure

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,19 @@
+import * as S from "./styles";
+import Logo from "../../assets/logo.png";
+
+function Navbar() {
+  return (
+    <S.Navbar>
+      <S.Container>
+        <S.LeftColumn>
+          <img src={Logo} alt="Ribon Logo" width={100}/>
+        </S.LeftColumn>
+        <S.RightColumn>
+          <S.DiscordCTA href="#">Subscribe</S.DiscordCTA>
+        </S.RightColumn>
+      </S.Container>
+    </S.Navbar>
+  );
+}
+
+export default Navbar;
